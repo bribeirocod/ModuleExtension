42c491698760051d726cb3396c19722792af8cf1
Bruno Henrique <brunohbr123@gmail.com>
2020-08-20 12:25:42 -0300

    first commit

---
+++ ModuleExtension/Extension.switf.swift
@@ -0,0 +1,15 @@
+//
+//  Extension.switf.swift
+//  ModuleExtension
+//
+//  Created by Bruno Henrique Brito Ribeiro on 20/08/20.
+//  Copyright © 2020 Bruno Henrique Brito Ribeiro. All rights reserved.
+//
+
+import Foundation
+
+public struct Extensions {
+    public func get() -> String{
+        return "Module Extension HardCode Value"
+    }
+}
---
+++ ModuleExtension/ModuleExtension.h
@@ -0,0 +1,19 @@
+//
+//  ModuleExtension.h
+//  ModuleExtension
+//
+//  Created by Bruno Henrique Brito Ribeiro on 20/08/20.
+//  Copyright © 2020 Bruno Henrique Brito Ribeiro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleExtension.
+FOUNDATION_EXPORT double ModuleExtensionVersionNumber;
+
+//! Project version string for ModuleExtension.
+FOUNDATION_EXPORT const unsigned char ModuleExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleExtension/PublicHeader.h>
+
+
